f4ee3a64580fe768f625a7bcde090901f6989f9b
Brad Jordan <badgerbaj@gmail.com>
2017-01-28 12:46:59 -0500

    Added the bulk of the required classes most of them are still incomplete

---
+++ app/src/main/java/com/toddbray/blackjack/Deck.java
@@ -0,0 +1,120 @@
+package com.toddbray.blackjack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by Brad on 1/28/2017.
+ *
+ */
+
+public class Deck extends MainActivity {
+
+    private HashMap<Integer, Integer> deck;
+    //private ArrayList<Integer> shuffleOrder;
+    private int[] shuffleOrder;
+    private int deckPosition;
+
+
+    public HashMap<Integer, Integer> getDeck() {
+        return deck;
+    }
+
+    public int[] getShuffleOrder() {
+        return shuffleOrder;
+    }
+
+    public int getDeckPosition() {
+        return deckPosition;
+    }
+
+    public void setDeck(HashMap<Integer, Integer> deck) {
+        this.deck = deck;
+    }
+
+    public void setShuffleOrder(int[] shuffleOrder) {
+        this.shuffleOrder = shuffleOrder;
+    }
+
+    public void setDeckPosition(int deckPosition) {
+        this.deckPosition = deckPosition;
+    }
+
+    public Deck() {
+        this.deck = new HashMap<>();
+        this.deck.put(1, R.drawable.ace_clubs);
+        this.deck.put(2, R.drawable.two_clubs);
+        this.deck.put(3, R.drawable.three_clubs);
+        this.deck.put(4, R.drawable.four_clubs);
+        this.deck.put(5, R.drawable.five_clubs);
+        this.deck.put(6, R.drawable.six_clubs);
+        this.deck.put(7, R.drawable.seven_clubs);
+        this.deck.put(8, R.drawable.eight_clubs);
+        this.deck.put(9, R.drawable.nine_clubs);
+        this.deck.put(10, R.drawable.ten_clubs);
+        this.deck.put(11, R.drawable.jack_clubs);
+        this.deck.put(12, R.drawable.queen_clubs);
+        this.deck.put(13, R.drawable.king_clubs);
+        this.deck.put(14, R.drawable.ace_spades);
+        this.deck.put(15, R.drawable.two_spades);
+        this.deck.put(16, R.drawable.three_spades);
+        this.deck.put(17, R.drawable.four_spades);
+        this.deck.put(18, R.drawable.five_spades);
+        this.deck.put(19, R.drawable.six_spades);
+        this.deck.put(20, R.drawable.seven_spades);
+        this.deck.put(21, R.drawable.eight_spades);
+        this.deck.put(22, R.drawable.nine_spades);
+        this.deck.put(23, R.drawable.ten_spades);
+        this.deck.put(24, R.drawable.jack_spades);
+        this.deck.put(25, R.drawable.queen_spades);
+        this.deck.put(26, R.drawable.king_spades);
+        this.deck.put(27, R.drawable.ace_hearts);
+        this.deck.put(28, R.drawable.two_hearts);
+        this.deck.put(29, R.drawable.three_hearts);
+        this.deck.put(30, R.drawable.four_hearts);
+        this.deck.put(31, R.drawable.five_hearts);
+        this.deck.put(32, R.drawable.six_hearts);
+        this.deck.put(33, R.drawable.seven_hearts);
+        this.deck.put(34, R.drawable.eight_hearts);
+        this.deck.put(35, R.drawable.nine_hearts);
+        this.deck.put(36, R.drawable.ten_hearts);
+        this.deck.put(37, R.drawable.jack_hearts);
+        this.deck.put(38, R.drawable.queen_hearts);
+        this.deck.put(39, R.drawable.king_hearts);
+        this.deck.put(40, R.drawable.ace_diamonds);
+        this.deck.put(41, R.drawable.two_diamonds);
+        this.deck.put(42, R.drawable.three_diamonds);
+        this.deck.put(43, R.drawable.four_diamonds);
+        this.deck.put(44, R.drawable.five_diamonds);
+        this.deck.put(45, R.drawable.six_diamonds);
+        this.deck.put(46, R.drawable.seven_diamonds);
+        this.deck.put(47, R.drawable.eight_diamonds);
+        this.deck.put(48, R.drawable.nine_diamonds);
+        this.deck.put(49, R.drawable.ten_diamonds);
+        this.deck.put(50, R.drawable.jack_diamonds);
+        this.deck.put(51, R.drawable.queen_diamonds);
+        this.deck.put(52, R.drawable.king_diamonds);
+
+        shuffleOrder = new int[CARD_COUNT];
+        shuffleOrder = shuffle();
+    }
+
+    private int[] shuffle(){
+        Random rn = new Random();
+        int[] shuffle = new int[CARD_COUNT];
+        int newNum;
+
+        for(int i = 0; i < CARD_COUNT; i++){
+            newNum = rn.nextInt(CARD_COUNT) + 1;
+            while (!Arrays.asList(shuffle).contains(newNum)) {
+                newNum = rn.nextInt(CARD_COUNT) + 1;
+            }
+
+            shuffle[i] = newNum;
+        }
+        return shuffle;
+    }
+}
--- app/src/main/java/com/toddbray/blackjack/MainActivity.java
+++ app/src/main/java/com/toddbray/blackjack/MainActivity.java
@@ -1,13 +1,40 @@
 package com.toddbray.blackjack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public static final String SHUFFLE_KEY = "shuffle";
+    public static final String DECK_POSITION_KEY = "deckposition";
+    public final int CARD_COUNT = 52;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bet);
+        setContentView(R.layout.activity_main);
+
+        int[] buttonIds = {R.id.play_button};
+        for( int id : buttonIds)
+        {
+            Button b = (Button) findViewById(id);
+            b.setOnClickListener(this);
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.play_button:
+                // Call About_App
+                Intent iActivity_Bet = new Intent(getApplicationContext(), activity_bet.class);
+                startActivity(iActivity_Bet);
+                break;
+
+        }
     }
 }

6fae40daa4aa3c2c3a5845397e5cbf63d90479a1
Todd <Toddab16@gmail.com>
2017-01-29 12:57:51 -0500

    Title screen complete with image and 2 buttons

--- app/src/main/java/com/toddbray/blackjack/MainActivity.java
+++ app/src/main/java/com/toddbray/blackjack/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bet);
+        setContentView(R.layout.actvity_game);
     }
 }
